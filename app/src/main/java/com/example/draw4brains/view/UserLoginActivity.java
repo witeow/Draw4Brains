5d7584547c17ae03379d51d28885b9c330991791
Russellleung <35394164+Russellleung@users.noreply.github.com>
2021-09-09 22:24:27 +0800

    fix

--- app/src/main/java/com/example/draw4brains/view/UserLoginActivity.java
+++ app/src/main/java/com/example/draw4brains/view/UserLoginActivity.java
@@ -1,4 +1,27 @@
 package com.example.draw4brains.view;
 
-public class UserLoginActivity {
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+
+import com.example.draw4brains.R;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class UserLoginActivity extends AppCompatActivity {
+
+    EditText email;
+    EditText firstname;
+    EditText lastname;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_login);
+
+
+
+
+    }
 }
